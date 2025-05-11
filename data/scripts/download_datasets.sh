1963c6f1321867c9fdcfbaeee804be8d6e1770e4
Sergey Zelenovskii <zelnovskiy254@gmail.com>
2025-05-11 17:37:08 +0300

    feat: remove elsa d3

--- data/scripts/download_datasets.sh
+++ data/scripts/download_datasets.sh
@@ -18,4 +18,4 @@ uv run $KAGGLE_SCRIPT -d selfishgene/sfhq-t2i-synthetic-faces-from-text-2-image-
 uv run $KAGGLE_SCRIPT -d alessandrasala79/ai-vs-human-generated-dataset -p $DATA_ROOT/datasets/ai_vs_human_generated --unzip
 uv run $KAGGLE_SCRIPT -d mohannadaymansalah/stable-diffusion-dataaaaaaaaa -p $DATA_ROOT/datasets/sd_faces --unzip
 uv run $KAGGLE_SCRIPT -d programmerrdai/open-images-v6 -p $DATA_ROOT/datasets/openimages_v6 --unzip
-uv run $ELSA_D3_SCRIPT $ELSA_D3_N_FILES --dest $DATA_ROOT/datasets/slice_elsa_d3
+# uv run $ELSA_D3_SCRIPT $ELSA_D3_N_FILES --dest $DATA_ROOT/datasets/slice_elsa_d3
